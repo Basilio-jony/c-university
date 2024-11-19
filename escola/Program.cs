345398ef64cb027bb058bea08fb64f82ef1592b2
Basilio-jony <johnyjony76@gmail.com>
2024-11-19 16:40:00 +0100

    adicionado_comentario

--- escola/Program.cs
+++ escola/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic;//usamos para trabalhar com listas de objectos como a lista <professor>
 
 public class Disciplina
 {
